a33c9c413f24593b41e795b0ec3c868043cac424
shotlom <sholto.maud@gmail.com>
2014-03-09 04:42:31 -0400

    added mongoose locally as it did not seems to work -g, got rid of test file

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var http = require('http');
+var http = require('http'),
+	mongoose = require('mongoose'),
+	format = require('util').format;
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+var db =mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open',function callback(){
+	//all good
+	console.log("made connection to mongodb/test");
+});
 
 var server = http.createServer(function (request, response){
 	response.writeHead(200, {"Content-Type": "text/plain"});
